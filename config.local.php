b6d822e256a1e1d8e89a51a18951a39b6526e439
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-11-13 07:21:50 -0600

    fix: pagseguro bug

--- config.local.php
+++ config.local.php
@@ -6,8 +6,8 @@ $config['mail_enabled'] = true;
 $config['server_path'] = '/home/noctera_user/canary/';
 $config['mail_admin'] = 'jalomo.chavez18@outlook.com';
 $config['mail_address'] = 'jalomo.chavez18@outlook.com';
-$config['date_timezone'] = 'America/Monterrey';
-$config['client'] = '1330';
+$config['date_timezone'] = 'America/Chicago';
+$config['client'] = '1340';
 $config['session_prefix'] = 'myaac_v9lyxflp_';
 $config['cache_prefix'] = 'myaac_ok5xu3lk_';
 
