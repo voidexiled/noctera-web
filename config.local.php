2a203feae1934d62357ea0a2f2327942f4fd9e4d
root <root@noctera-global.us-central1-c.c.dev-server-439404.internal>
2024-11-20 14:48:45 +0000

    modified

--- config.local.php
+++ config.local.php
@@ -3,8 +3,8 @@
 $config['installed'] = true;
 $config['env'] = 'prod'; // dev or prod
 $config['mail_enabled'] = true;
-//$config['server_path'] = '/home/noctera_user/canary/';
-$config['server_path'] = 'C:\Users\jalomo\Downloads\13.41 server\canary';
+$config['server_path'] = '/home/noctera_user/canary/';
+//$config['server_path'] = 'C:\Users\jalomo\Downloads\13.41 server\canary';
 $config['mail_admin'] = 'jalomo.chavez18@outlook.com';
 $config['mail_address'] = 'jalomo.chavez18@outlook.com';
 $config['date_timezone'] = 'America/Chicago';
--- config.php
+++ config.php
@@ -311,7 +311,7 @@ $config = array(
 
 	// status bar
 	'status_bar' => true,
-	'client_link' => 'https://github.com/voidexiled/noctera-launcher/releases/download/v0.0.0.0-BETA/Noctera.Global.Launcher.v1.0.rar', // link to download tibia client
+	'client_link' => 'https://github.com/voidexiled/noctera-launcher/releases/download/v0.0.0.0-BETA/Noctera.Global.Launcher.v1.1.zip', // link to download tibia client
 	'discord_link' => 'https://discord.gg/4KzZMApf', // link to join discord channel
 	'whatsapp_link' => '', // wa.me/5511912345678
 	'instagram_link' => 'profile', // www.instagram.com/profile
@@ -389,5 +389,5 @@ $config = array(
         'words' => [],
     ],
 
-    'enablePagseguroLocal' => false, // set true to enable donate and boxes page on localhost.
+    'enablePagseguroLocal' => true, // set true to enable donate and boxes page on localhost.
 );
