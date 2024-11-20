fb946139ed9036123286e35d55e1f9b0f7116e0c
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-11-20 09:18:26 -0600

    rollback

--- system/login.php
+++ system/login.php
@@ -119,7 +119,6 @@ if (ACTION === 'logout' && !isset($_REQUEST['account_login'])) {
                 // temporary solution for blocking failed login attempts
                 if ($cache->enabled()) {
                     if (isset($t)) {
-                        $t = array('attempts' => 1, 'last' => time());
                         $t['attempts']++;
                         $t['last'] = time();
 
